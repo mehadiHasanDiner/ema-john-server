7339df076dda0f2578c9e294c3d3aed5c967b194
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-03-28 07:33:27 +0600

    everything done

--- index.js
+++ index.js
@@ -19,33 +19,46 @@ const port = 5000;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productCollection = client.db("emaJohnShop").collection("products");
+  const ordersCollection = client.db("emaJohnShop").collection("orders");
 
   app.post('/addProduct', (req, res) => {
     const products = req.body;
     productCollection.insertMany(products)
-    .then(result => {
+      .then(result => {
         console.log(result.insertedCount);
         res.send(result.insertedCount)
-    })
+      })
   })
 
   app.get('/products', (req, res) => {
-      productCollection.find({})
+    productCollection.find({})
       .toArray((err, documents) => {
-          res.send(documents);
+        res.send(documents);
       })
   })
 
   app.get('/product/:key', (req, res) => {
-    productCollection.find({key: req.params.key})
-    .toArray((err, documents) => {
+    productCollection.find({ key: req.params.key })
+      .toArray((err, documents) => {
         res.send(documents[0]);
-    })
-})
+      })
+  })
 
-app.post('/productByKeys', (req, res) => {
-    
-})
+  app.post('/productByKeys', (req, res) => {
+    const productKeys = req.body;
+    productCollection.find({ key: { $in: productKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order)
+    .then(result => {
+        res.send(result.insertedCount > 0)
+    })
+  })
 
 
 });
